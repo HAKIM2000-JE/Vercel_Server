a017a58c7e29d49cbffbc47d60b10e4fef5b6181
AbdelHakim Jebabra <abdelhakimjebabra@iMac-de-GodMode.local>
2023-05-23 10:44:53 +0200

    use temp directory

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.post('/handleTweets', async (req, res) => {
     // Post the image as a tweet
     const twitterClient = client.readWrite;
   
-    download(body.image, "Example", async function () {
+    download(body.image, "/tmp/image.png", async function () {
       try {
-        const mediaId = await twitterClient.v1.uploadMedia('./image.png');
+        const mediaId = await twitterClient.v1.uploadMedia("/tmp/image.png");
         await twitterClient.v2.tweet({
           text: body.userComment,
           media: {
@@ -57,7 +57,7 @@ function download(uri, filename, callback) {
     // Use your preferred method to download the image
     // For example, using the 'request' module
     request.head(uri, function (err, res, body) {
-      request(uri).pipe(fs.createWriteStream(`/tmp/${filename}`)).on('close', callback);
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   }
   
